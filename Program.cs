b01d69d1d0f7851c754a642e89f7c1944b6c2c81
Lars Nyström <lars.nystrom@mio.se>
2021-04-21 23:35:26 +0200

    Playing with config

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace ConfigSandbox
 {
@@ -6,7 +8,26 @@ namespace ConfigSandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true)
+                .AddCommandLine(args)
+                .Build();
+
+            System.Console.WriteLine(config["string"]);
+            System.Console.WriteLine(config["stringfromappsettings"]);
+            
+            List<Stuff> l = new List<Stuff>();
+            config.GetSection("array").Bind(l);
+            l.ForEach(a => System.Console.WriteLine(a.Name));
+
+            var cliArray = new List<Stuff>();
+            config.GetSection("cliarray").Bind(cliArray);
+            cliArray.ForEach(a => System.Console.WriteLine(a.Name));
         }
+    }
+
+    internal class Stuff
+    {
+        public string Name { get; set; }
     }
 }
