b34003644c752e4d23ecd416694526a18891adc1
Lars Nyström <lars.nystrom@mio.se>
2021-05-11 14:18:55 +0200

    Play with label config

--- Program.cs
+++ Program.cs
@@ -13,8 +13,13 @@ namespace ConfigSandbox
                 .AddCommandLine(args)
                 .Build();
 
+            IConfiguration labelsConfig = new ConfigurationBuilder()
+                .AddJsonFile(config["labelconfig"], true)
+                .Build();
+
             System.Console.WriteLine(config["string"]);
             System.Console.WriteLine(config["stringfromappsettings"]);
+            System.Console.WriteLine(config["labelconfig"]);
             
             List<Stuff> l = new List<Stuff>();
             config.GetSection("array").Bind(l);
@@ -23,11 +28,23 @@ namespace ConfigSandbox
             var cliArray = new List<Stuff>();
             config.GetSection("cliarray").Bind(cliArray);
             cliArray.ForEach(a => System.Console.WriteLine(a.Name));
+
+            System.Console.WriteLine("Label images:");
+            var labelImages = new List<LabelImage>();
+            labelsConfig.GetSection("labelImages").Bind(labelImages);
+            labelImages.ForEach(i => System.Console.WriteLine($"{i.Filename} - ({i.PositionX},{i.PositionY})"));
         }
     }
 
     internal class Stuff
     {
         public string Name { get; set; }
+    }
+
+    internal class LabelImage
+    {
+        public string Filename { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
     }
 }
